95ef0dc444f0c62f2db33df8944dbf7bd8d73733
midbel <brun.nicolas21@gmail.com>
2020-06-16 12:16:18 +0200

    api skeleton

---
+++ src/fmt.cpp
@@ -0,0 +1,19 @@
+#include "fmt.hpp"
+
+namespace fmt
+{
+  string sprintf(string format)
+  {
+    return "";
+  }
+
+  void printf(string format)
+  {
+
+  }
+
+  void fprintf(ostream os, string format)
+  {
+    
+  }
+}
---
+++ src/fmt.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+namespace fmt
+{
+  string sprintf(string format);
+  void printf(string format);
+  void fprintf(ostream os, string format);
+}
